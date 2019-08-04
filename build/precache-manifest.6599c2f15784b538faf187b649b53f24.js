bc9352d03a89450e1aee7c1a30e8e30fc481e0fe
Albert <albertp1207@mail.ru>
2019-08-04 22:16:49 +0400

    OoO

---
+++ build/precache-manifest.6599c2f15784b538faf187b649b53f24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d914d496498361ab15189d10f5c926f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a7f68fdb87dea654840",
+    "url": "/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "56cd17dc793cccca3520",
+    "url": "/static/js/2.51d8c421.chunk.js"
+  },
+  {
+    "revision": "5a7f68fdb87dea654840",
+    "url": "/static/js/main.8def25a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
